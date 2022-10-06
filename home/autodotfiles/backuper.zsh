8eec78f709e13c100a64d37f4e4b667c7cf56085
Clément Nerma <clement.nerma@gmail.com>
2022-10-06 17:22:50 +0200

    Improve the obfuscator :rocket:

--- home/autodotfiles/backuper.zsh
+++ home/autodotfiles/backuper.zsh
@@ -8,10 +8,16 @@
 # Requires to set the "ADF_LOCBAK_PASSPHRASE" variable
 # Arguments are paths to back up
 # Set "ADF_ADD_ADF_FILES_TO_BACKUP" to perform a backup of the current environment
+# You may also set "ADF_DEOBFUSCATE_PASSPHRASE"
 function adf_local_backup() {
     if [[ -z $ADF_LOCBAK_PASSPHRASE ]]; then
         echoerr "Please provide a \z[yellow]°\$ADF_LOCBAK_PASSPHRASE\z[]° variable."
         return 1
+    fi
+    
+    if (( $ADF_DEOBFUSCATE_PASSPHRASE )); then
+        ADF_DEOBFUSCATE_PASSPHRASE=$(adf_obf_decode "$ADF_DEOBFUSCATE_PASSPHRASE")
+        if [[ $? != 0 ]]; then return 2; fi
     fi
 
     echoinfo "(1/3) Building the files list..."
@@ -24,8 +30,8 @@ function adf_local_backup() {
         _adf_add_dir_to_list "$listfile" "$ADF_LAST_BACKUP_DIR"
     fi
 
-    if ! _adf_add_dir_to_list "$listfile" "$@"; then return 2; fi
-    if ! _adf_add_dir_to_list "$listfile" $ADF_ALWAYS_BACKUP; then return 2; fi
+    if ! _adf_add_dir_to_list "$listfile" "$@"; then return 3; fi
+    if ! _adf_add_dir_to_list "$listfile" $ADF_ALWAYS_BACKUP; then return 3; fi
 
     echoinfo ""
     echoinfo "(2/3) Compressing \z[yellow]°$(wc -l < "$listfile")\z[]° elements..."
@@ -36,7 +42,7 @@ function adf_local_backup() {
     if ! 7z a -t7z -m0=lzma2 -mx=5 -mfb=64 -md=32m -ms=on -mhc=on -mhe=on -spf2 -bso0 -p"$ADF_LOCBAK_PASSPHRASE" "$tmpfile" @"$listfile"; then
         echoerr "Command \z[yellow]°7z\z[]° failed."
         command rm "$listfile"
-        return 3
+        return 4
     fi
 
     command rm "$listfile"
@@ -49,7 +55,7 @@ function adf_local_backup() {
 
     if ! mv "$tmpfile" "$outfile"; then
         echoerr "Command \z[yellow]°mv\z[]° failed."
-        return 4
+        return 5
     fi
 
     echosuccess "Done! Archive path is \z[magenta]°$outfile\z[]°"
