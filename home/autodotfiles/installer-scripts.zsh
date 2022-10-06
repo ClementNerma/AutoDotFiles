b30c653a5488cd7769882c8c850a9fbd2fa5355a
Clément Nerma <clement.nerma@gmail.com>
2022-10-06 17:23:13 +0200

    Update hardcoded version of installable NCDU

--- home/autodotfiles/installer-scripts.zsh
+++ home/autodotfiles/installer-scripts.zsh
@@ -154,7 +154,7 @@ function ncdu() {
     # NEEDS_APT_UPDATE: no
 
     # TODO: Find a way to not hardcode NCDU's version and link here
-    dl "https://dev.yorhel.nl/download/ncdu-2.0-beta2-linux-x86_64.tar.gz" "$INSTALLER_TMPDIR/ncdu.tar.gz"
+    dl "https://dev.yorhel.nl/download/ncdu-2.0-linux-x86_64.tar.gz" "$INSTALLER_TMPDIR/ncdu.tar.gz"
     tar zxf "$INSTALLER_TMPDIR/ncdu.tar.gz" -C "$ADF_BIN_DIR"
 }
 
