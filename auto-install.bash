b0acc2cd5d94fa26017ee34adb5983992b7eaeca
Clément Nerma <clement.nerma@gmail.com>
2022-10-06 17:22:38 +0200

    Update temporary directory location in auto installer

--- auto-install.bash
+++ auto-install.bash
@@ -45,7 +45,7 @@ CURRENT_STEP=0
 AUTO_INSTALLER_STARTED_AT=$(date +%s)
 
 # Choose a temporary directory
-export TMPDIR="/tmp/_auto_installer"
+export TMPDIR="/tmp/_setupenv_autoinstaller_$AUTO_INSTALLER_STARTED_AT"
 
 # Beginning of the installer!
 echo
