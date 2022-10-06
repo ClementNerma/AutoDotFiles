7b904635e56a21f18138057c48e91bebd71269a6
Clément Nerma <clement.nerma@gmail.com>
2022-10-06 17:22:49 +0200

    Fix: unterminated color groups in installers' warning messages

--- home/autodotfiles/installer/scripts/all/fzf.zsh
+++ home/autodotfiles/installer/scripts/all/fzf.zsh
@@ -1,6 +1,6 @@
 if [[ -d ~/.fzf ]]; then
 	mvbak ~/.fzf
-	echoc "\z[yellow]°\!/ A previous version of \z[green]°Fuzzy Finder\z[]° was detected ==> backed it up to \z[magenta]°$LAST_FILEBAK_PATH\z[]°..."
+	echoc "\z[yellow]°\!/ A previous version of \z[green]°Fuzzy Finder\z[]° was detected ==> backed it up to \z[magenta]°$LAST_FILEBAK_PATH\z[]°...\z[]°"
 fi
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
