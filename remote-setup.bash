7f4262b538a434457f20a23d9a74ec385e1e1ae4
Clément Nerma <clement.nerma@gmail.com>
2022-10-06 17:22:55 +0200

    Wrap 'echo' messages in double quotes

--- remote-setup.bash
+++ remote-setup.bash
@@ -23,9 +23,9 @@ echo "= REMOTE SETUP INSTALLER ="
 echo "=                        ="
 echo "=========================="
 echo
-echo This installer will setup a fresh computer or server remotely.
+echo "This installer will setup a fresh computer or server remotely."
 echo
-echo First, please indicate the remote computer\'s IP address:
+echo "First, please indicate the remote computer\'s IP address:"
 echo
 
 if [ -z "$2" ]; then
@@ -36,9 +36,9 @@ else
 fi
 
 echo
-echo Now please choose the remote user to setup the server for.
-echo Note that many packages will be installed for all users, but most of the customization will only
-echo  be applied to the user you choose.
+echo "Now please choose the remote user to setup the server for."
+echo "Note that many packages will be installed for all users, but most of the customization will only"
+echo " be applied to the user you choose."
 echo
 
 if [ -z "$1" ]; then
@@ -49,31 +49,31 @@ else
 fi
 
 echo
-echo Everything will be installed for user \"$user_name\" on computer at address \"$computer_ip\".
+echo "Everything will be installed for user \"$user_name\" on computer at address \"$computer_ip\"."
 echo
 
 if [[ "$3" != "-y" && "$3" != "--yes" ]]; then
 	read -p "Please press <Enter> to continue or <Ctrl+C> to cancel installation."
 	read -p "Are you sure? Press <Enter> to confirm or <Ctrl+C> to abort."
 else
-	echo Got confirmation from command line flag "('--y' or '--yes' flag was supplied)".
+	echo "Got confirmation from command line flag ('--y' or '--yes' flag was supplied)".
 fi
 
 echo
-echo Here we go\!
+echo "Here we go!"
 
-_step Step 1/3: Checking if the remote hots can be reached correctly...
+_step "Step 1/3: Checking if the remote hots can be reached correctly..."
 ssh $user_name@$computer_ip echo Connected successfully.
 
-_step Step 2/3: Copying files...
+_step "Step 2/3: Copying files..."
 ssh $user_name@$computer_ip rm -rf .___remote_setup
 ssh $user_name@$computer_ip mkdir .___remote_setup
 scp -r "$SCRIPT_DIR/home" $user_name@$computer_ip:./.___remote_setup
 scp "$SCRIPT_DIR/auto-install.bash" $user_name@$computer_ip:./.___remote_setup
 
-_step Step 3/3: Running installer...
+_step "Step 3/3: Running installer..."
 ssh $user_name@$computer_ip bash .___remote_setup/auto-install.bash
 
-_step Done\!
+_step "Done!"
 
-echo You may now enjoy your setup computer ":)"
+echo "You may now enjoy your setup computer :)"
