79f5d22b16fdc1e56bdd08f2cc0157e6581626a7
Clément Nerma <clement.nerma@gmail.com>
2022-10-06 17:22:52 +0200

    Fix: unterminated color groups

--- home/autodotfiles/installer/scripts/all/pre/rust.zsh
+++ home/autodotfiles/installer/scripts/all/pre/rust.zsh
@@ -1,11 +1,11 @@
 if [ -d ~/.rustup ]; then
 	mvbak ~/.rustup
-	echowarn "\z[yellow]°\!/ A previous version of \z[green]°Rust was detected ==> backed it up to \z[magenta]°$LAST_FILEBAK_PATH\z[]°...\z[]°"
+	echowarn "\z[yellow]°\!/ A previous version of \z[green]°Rust\z[]° was detected ==> backed it up to \z[magenta]°$LAST_FILEBAK_PATH\z[]°...\z[]°"
 fi
 
 if [ -d ~/.cargo ]; then
 	mvbak ~/.cargo
-	echowarn "\z[yellow]°\!/ A previous version of \z[green]°Cargo was detected ==> backed it up to \z[magenta]°$LAST_FILEBAK_PATH\\z[]°..\z[]°"	
+	echowarn "\z[yellow]°\!/ A previous version of \z[green]°Cargo\z[]° was detected ==> backed it up to \z[magenta]°$LAST_FILEBAK_PATH\\z[]°..\z[]°"	
 fi
 
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable -y
