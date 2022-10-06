e78df041f688901787744929d2e289f7090d7f34
Clément Nerma <clement.nerma@gmail.com>
2022-10-06 17:22:37 +0200

    Install AtomicParsley for thumbnails embedding with Youtube-DL

--- auto-install.bash
+++ auto-install.bash
@@ -223,6 +223,9 @@ _step "Installing Youtube-DL..."
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
 
+_step "Installing AtomicParsley for Youtube-DL..."
+sudo apt install atomicparsley
+
 _step "Installing utilities..."
 sudo apt install -y pv htop ncdu net-tools rsync
 
