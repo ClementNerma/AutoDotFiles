fa5a972f097b32773559b6147f64e57f8036048e
Clément Nerma <clement.nerma@gmail.com>
2022-10-06 17:22:42 +0200

    Fix: use correct grepper for Tokei on AMD64/ARM64 platforms

--- home/zsh-sub/installer/scripts/all/tokei.zsh
+++ home/zsh-sub/installer/scripts/all/tokei.zsh
@@ -1,12 +1,11 @@
 
 if [[ $(dpkg --print-architecture) = "arm64" ]]; then
-	_TOKEI_GREPPER="tokei-x86_64-unknown-linux-gnu.tar.gz"
-else
 	_TOKEI_GREPPER="tokei-aarch64-linux-android.tar.gz"
+else
+	_TOKEI_GREPPER="tokei-x86_64-unknown-linux-gnu.tar.gz"
 fi
 
 dlghrelease "XAMPPRocky/tokei" "$_TOKEI_GREPPER" "$INSTALLER_TMPDIR/tokei.tar.gz"
-
 tar zxf "$INSTALLER_TMPDIR/tokei.tar.gz"
 
 sudo mv tokei /usr/local/bin
