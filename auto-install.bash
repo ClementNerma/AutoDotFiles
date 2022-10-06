794bd2902240f6167e053617be8447077c0cf028
Clément Nerma <clement.nerma@gmail.com>
2022-10-06 17:22:36 +0200

    Fix: don't fail if Fuzzy Finder is already installed

--- auto-install.bash
+++ auto-install.bash
@@ -197,8 +197,13 @@ else
 fi
 
 _step "Installing Fuzzy Finder..."
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --all
+if [[ -d ~/.fzf ]]; then
+	echo -e "\e[33m\!/ A previous version of \e[32mFuzzy Finder \e[33mwas detected ==> backing it up to \e[32m~/.fzf.bak\e[33m...\e[0m"
+	mv ~/.fzf ~/.fzf.bak
+else
+	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+	~/.fzf/install --all
+fi
 
 _step "Installing Trasher..."
 if [[ $arch != "armhf" ]]; then
