903e6b3abea400a2dabc388d97d4a6978ae92ee7
Clément Nerma <clement.nerma@gmail.com>
2022-10-06 17:22:44 +0200

    Add support for 'yq' on ARM64

--- home/zsh-sub/installer/scripts/all/yq.zsh
+++ home/zsh-sub/installer/scripts/all/yq.zsh
@@ -1,3 +1,8 @@
-dlghrelease "mikefarah/yq" "yq_linux_amd64" "$INSTALLER_TMPDIR/yq"
+if [[ $(dpkg --print-architecture) = "arm64" ]]; then
+    dlghrelease "mikefarah/yq" "yq_linux_arm64" "$INSTALLER_TMPDIR/yq"
+else
+    dlghrelease "mikefarah/yq" "yq_linux_amd64" "$INSTALLER_TMPDIR/yq"
+fi
+
 chmod +x "$INSTALLER_TMPDIR/yq"
 sudo mv "$INSTALLER_TMPDIR/yq" /usr/local/bin/yq
