30da552b06c07dbb8343cd308babf87706a7df06
Clément Nerma <clement.nerma@gmail.com>
2022-10-06 17:22:54 +0200

    Fix: exa installer for ARM64

--- home/autodotfiles/installer/scripts/all/exa.zsh
+++ home/autodotfiles/installer/scripts/all/exa.zsh
@@ -1,10 +1,13 @@
 if [[ $(dpkg --print-architecture) = "arm64" ]]; then
-	cargo install exa
-	return
+	_EXA_GREPPER="armv7"
+else
+	_EXA_GREPPER="x86_64"
 fi
 
-dlghrelease "ogham/exa" "exa-linux-x86_64-.*.zip" "$INSTALLER_TMPDIR/exa.zip"
+dlghrelease "ogham/exa" "exa-linux-$_EXA_GREPPER-.*.zip" "$INSTALLER_TMPDIR/exa.zip"
 
 unzip "$INSTALLER_TMPDIR/exa.zip" -d "$INSTALLER_TMPDIR/exa"
 
 mv "$INSTALLER_TMPDIR/exa/bin/exa" $ADF_BIN_DIR/exa
+
+unset _EXA_GREPPER
