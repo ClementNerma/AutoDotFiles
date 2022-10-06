36d27929fb69c762f7ba7838aa8024468cbe9e01
Clément Nerma <clement.nerma@gmail.com>
2022-10-06 17:23:18 +0200

    Remove startup script for now

--- home/autodotfiles/startup.zsh
+++ home/autodotfiles/startup.zsh
@@ -1,13 +1,16 @@
 
-function __adf_after_boot() {
-    echo "Starting up..."
-    ytsync_unlock_all
-}
+# Removed until a reliable way to check uptime is found
+# (`uptime -s` provides inconsistent values)
 
-export ADF_LAST_REBOOT=$(uptime -s)
-export ADF_LAST_REBOOT_FILE="$ADF_ASSETS_DIR/last-reboot"
+# function __adf_after_boot() {
+#     echo "Starting up..."
+#     ytsync_unlock_all
+# }
 
-if [[ ! -f $ADF_LAST_REBOOT_FILE ]] || [[ $ADF_LAST_REBOOT != $(command cat "$ADF_LAST_REBOOT_FILE") ]]; then
-    echo "$ADF_LAST_REBOOT" > "$ADF_LAST_REBOOT_FILE"
-    __adf_after_boot
-fi
+# export ADF_LAST_REBOOT=$(uptime -s)
+# export ADF_LAST_REBOOT_FILE="$ADF_ASSETS_DIR/last-reboot"
+
+# if [[ ! -f $ADF_LAST_REBOOT_FILE ]] || [[ $ADF_LAST_REBOOT != $(command cat "$ADF_LAST_REBOOT_FILE") ]]; then
+#     echo "$ADF_LAST_REBOOT" > "$ADF_LAST_REBOOT_FILE"
+#     __adf_after_boot
+# fi
