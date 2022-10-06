d58c240cee2da5783b2a025f335721d8c607d199
Clément Nerma <clement.nerma@gmail.com>
2022-10-06 17:22:42 +0200

    Fix: delete previous backup of FZF if exists

--- home/zsh-sub/installer/scripts/all/fzf.zsh
+++ home/zsh-sub/installer/scripts/all/fzf.zsh
@@ -1,6 +1,7 @@
 
 if [[ -d ~/.fzf ]]; then
 	echo -e "\e[33m\!/ A previous version of \e[32mFuzzy Finder\e[33m was detected ==> backing it up to \e[32m~/.fzf.bak\e[33m...\e[0m"
+	command rm -rf ~/.fzf.bak
 	mv ~/.fzf ~/.fzf.bak
 fi
 
