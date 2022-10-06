00e352e7f25a68a6e1e946fb2f091610cdb24d3b
Clément Nerma <clement.nerma@gmail.com>
2022-10-06 17:22:53 +0200

    Fix: download of component 'zoxide' in installer script

--- home/autodotfiles/installer/scripts/all/zoxide.zsh
+++ home/autodotfiles/installer/scripts/all/zoxide.zsh
@@ -1,9 +1,11 @@
 if [[ $(dpkg --print-architecture) = "arm64" ]]; then
 	_ZOXIDE_GREPPER="zoxide-armv7-unknown-linux-musleabihf"
 else
-	_ZOXIDE_GREPPER="zoxide-x86_64-unknown-linux-gnu"
+	_ZOXIDE_GREPPER="zoxide-x86_64-unknown-linux-musl"
 fi
 
-dlghrelease "ajeetdsouza/zoxide" "$_ZOXIDE_GREPPER" "$ADF_BIN_DIR/zoxide"
+dlghrelease "ajeetdsouza/zoxide" "$_ZOXIDE_GREPPER" "$INSTALLER_TMPDIR/zoxide.tar.gz"
+tar zxf "$INSTALLER_TMPDIR/zoxide.tar.gz" -C "$INSTALLER_TMPDIR"
+mv "$INSTALLER_TMPDIR/$_ZOXIDE_GREPPER/zoxide" "$ADF_BIN_DIR/zoxide"
 
 chmod +x "$ADF_BIN_DIR/zoxide"
