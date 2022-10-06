8486dd481909a678af6503ce16372a166b9b3f19
Clément Nerma <clement.nerma@gmail.com>
2022-10-06 17:22:41 +0200

    Add a 'zerc' command to edit and reload the local configuration file

--- home/zsh-sub/local/config.zsh
+++ home/zsh-sub/local/config.zsh
@@ -3,6 +3,9 @@
 # Its role is to declare all the configuration used by other ZSH files
 #
 
+# Edit this file and reload it
+alias zerc="nano ${(%):-%x} && source ${(%):-%x}"
+
 # Is this my main computer?
 # Set to '1' if it is
 #export ZSH_MAIN_PERSONAL_COMPUTER=0
