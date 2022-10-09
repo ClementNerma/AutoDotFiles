b8276151de4194deb3bbe2dfe83318261d90f64e
Clément Nerma <clement.nerma@gmail.com>
2022-10-09 16:41:52 +0200

    Accelerate job checking

--- home/autodotfiles/functions/bjobs.zsh
+++ home/autodotfiles/functions/bjobs.zsh
@@ -23,7 +23,7 @@ function bjlist() {
 function bjhas() {
     [[ -z $1 ]] && { echoerr "Please provide a task name"; return 1 }
 
-    if ! has_pid "_bjmarker $1" > /dev/null; then
+    if ! ps -ux | grep "_bjmarker $1 " | grep -v "grep" > /dev/null; then
         return 1
     fi
 }
@@ -33,15 +33,11 @@ function bjstart() {
     [[ -z $2 ]] && { echoerr "Please provide a command to run"; return 2 }
 
     if bjhas "$1"; then
-        if [[ $3 == "--ignore-running" ]]; then
-            return
-        fi
-
-        echowarn "This job is already running."
         return 1
     fi
 
     (nohup "$(which zsh)" "$ADF_ENTRYPOINT" "--just-run" _bjmarker "$1" "${@:2}" > /dev/null 2>&1 &)
+    echoinfo "Started task \z[yellow]°$1\z[]°."
 }
 
 function bjkill() {
