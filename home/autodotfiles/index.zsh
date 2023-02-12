24b107213154a4000b6f143cb4fe24ed846db0db
Clément Nerma <clement.nerma@gmail.com>
2023-02-12 21:27:53 +0100

    Remove reference to old directory

--- home/autodotfiles/index.zsh
+++ home/autodotfiles/index.zsh
@@ -38,9 +38,6 @@ if [[ ! -d $ADF_DATA_DIR ]]; then mkdir "$ADF_DATA_DIR"; fi
 if [[ ! -d $ADF_ASSETS_DIR ]]; then mkdir "$ADF_ASSETS_DIR"; fi
 if [[ ! -d $ADF_BIN_DIR ]]; then mkdir "$ADF_BIN_DIR"; fi
 
-# Set path to the external scripts directory
-export ADF_EXTERNAL_DIR="$ADF_DIR/external"
-
 # Set path to the files list
 export ADF_FILES_LIST="$HOME/.autodotfiles-files-list.txt"
 
