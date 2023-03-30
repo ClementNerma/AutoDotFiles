16ec9e72fc8594b9b1938f55c29e43cc367c101e
Clément Nerma <clement.nerma@gmail.com>
2023-03-30 11:18:42 +0200

    Allow to start by running a command

--- home/autodotfiles/index.zsh
+++ home/autodotfiles/index.zsh
@@ -149,6 +149,8 @@ if [[ $1 = "--just-run" && -n $2 ]]; then
 	if (( $ADF_JUST_RUN_RET )); then
 		return $ADF_JUST_RUN_RET
 	fi
+elif [[ $1 = "--start-with" && -n $2 ]]; then
+	${@:2}
 elif (( $ADF_CHECK_CRONY_FAILURES_STARTUP )); then
 	crony check
 fi
